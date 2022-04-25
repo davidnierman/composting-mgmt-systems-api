18ddd84ea7a689e2c55cf4dd7063a513b61da516
dnierman0920 <davidnierman93@gmail.com>
2022-04-25 07:07:03 -0700

    server is up and running

--- composting-mgmt-systems/wsgi.py
+++ composting-mgmt-systems/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_auth_template_template project.
+WSGI config for composting-mgmt-systems project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_auth_template.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'composting-mgmt-systems.settings')
 
 application = get_wsgi_application()
