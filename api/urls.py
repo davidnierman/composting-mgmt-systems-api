78f973a9a024aaaaf1c714f80162a2abc0d37963
dnierman0920 <davidnierman93@gmail.com>
2022-04-25 23:01:00 -0700

    add delete, update and show routes to locations

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from api.views.location_views import Locations
+from api.views.location_views import LocationDetail, Locations
 from .views.mango_views import Mangos, MangoDetail
 from .views.user_views import SignUp, SignIn, SignOut, ChangePassword
 
@@ -8,6 +8,7 @@ urlpatterns = [
   	# Restful routing
     path('mangos/', Mangos.as_view(), name='mangos'),
     path('locations/', Locations.as_view(), name='locations'),
+    path('locations/<int:pk>/', LocationDetail.as_view(), name = 'location_detail'),
     path('mangos/<int:pk>/', MangoDetail.as_view(), name='mango_detail'),
     path('sign-up/', SignUp.as_view(), name='sign-up'),
     path('sign-in/', SignIn.as_view(), name='sign-in'),
--- api/views/location_views.py
+++ api/views/location_views.py
@@ -2,7 +2,9 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics, status
 from rest_framework.response import Response
+from rest_framework.exceptions import PermissionDenied
 from django.http import JsonResponse # this will be better in the future using react
+from django.shortcuts import get_object_or_404
 
 
 from ..models.location import Location
@@ -31,3 +33,49 @@ class Locations(generics.ListCreateAPIView):
             location.save()
             return JsonResponse({ 'location': location.data }, status=status.HTTP_201_CREATED)
         return JsonResponse(location.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LocationDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes=(IsAuthenticated,)
+    def get(self, request, pk):
+        """Show request"""
+        # Locate the location to show
+        location = get_object_or_404(Location, pk=pk)
+        # Only want to show owned locations?
+        if request.user != location.user:
+            raise PermissionDenied('Unauthorized, you do not own this mango')
+
+        # Run the data through the serializer so it's formatted
+        data = LocationSerializer(location).data
+        return Response({ 'location': data })
+
+    def delete(self, request, pk):
+        """Delete request"""
+        # Locate location to delete
+        print('DELETE REQUEST RUNNNING')
+        location = get_object_or_404(Location, pk=pk)
+        # Check the location's user against the user making this request
+        if request.user != location.user:
+            raise PermissionDenied('Unauthorized, you do not own this location')
+        # Only delete if the user owns the location
+        location.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def partial_update(self, request, pk):
+        """Update Request"""
+        # Locate location
+        # get_object_or_404 returns a object representation of our Location
+        location = get_object_or_404(Location, pk=pk)
+        # Check the location's owner against the user making this request
+        if request.user != location.user:
+            raise PermissionDenied('Unauthorized, you do not own this location')
+
+        # Ensure the owner field is set to the current user's ID
+        request.data['location']['user'] = request.user.id
+        # Validate updates with serializer
+        data = LocationSerializer(location, data=request.data['location'], partial=True)
+        if data.is_valid():
+            # Save & send a 204 no content
+            data.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        # If the data is not valid, return a response with the errors
+        return Response(data.errors, status=status.HTTP_400_BAD_REQUEST)
